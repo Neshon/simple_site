62aa6352ee5e518627f54c8c17b77eb88c7f60d3
hifer11 <hifer11@yandex.ua>
2022-03-17 14:34:47 +0200

    add admin panel

--- crm/admin.py
+++ crm/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Order, StatusCrm, Comment
 
 
-admin.site.register(Order)
+class CommentAdmin(admin.StackedInline):
+    model = Comment
+    fields = ('dt', 'text')
+    readonly_fields = ('dt',)
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order_dt', 'order_name', 'order_phone', 'binding')
+    list_display_links = ('id', 'order_name')
+    search_fields = ('id', 'order_dt', 'order_name', 'order_phone')
+    list_filter = ('binding',)
+    list_editable = ('binding', 'order_phone')
+    list_per_page = 10
+    list_max_show_all = 100
+    fields = ('id', 'order_dt', 'order_name', 'order_phone', 'binding')
+    readonly_fields = ('id', 'order_dt')
+    inlines = [CommentAdmin, ]
+
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(StatusCrm)
 admin.site.register(Comment)
--- telegram_bot/sendmessage.py
+++ telegram_bot/sendmessage.py
@@ -3,13 +3,24 @@ from .models import TelegramSettings
 
 
 def sendmessage(name, phone):
-    settings = TelegramSettings.objects.get(pk=1)
-    token = str(settings.token)
-    chat_id = str(settings.chat_id)
-    text = str(settings.msg + '\nИмя: ' + name + '\nНомер телефона: ' + phone)
-    api = 'https://api.telegram.org/bot'
-    method = api + token + '/sendMessage'
+    if TelegramSettings.objects.get(pk=1):
+        settings = TelegramSettings.objects.get(pk=1)
+        token = str(settings.token)
+        chat_id = str(settings.chat_id)
+        api = 'https://api.telegram.org/bot'
+        method = api + token + '/sendMessage'
+        text = str(settings.msg + '\nИмя: ' + name + '\nНомер телефона: ' + phone)
 
-    req = requests.post(method,
-                        data={'chat_id': chat_id,
-                              'text': text})
+        try:
+            req = requests.post(method,
+                                data={'chat_id': chat_id,
+                                      'text': text})
+        finally:
+            if req.status_code != 200:
+                print('Ошибка отправки!')
+            elif req.status_code == 500:
+                print('Ошибка 500')
+            else:
+                print('Всё хорошо. Сообщение отправлено!')
+    else:
+        pass
